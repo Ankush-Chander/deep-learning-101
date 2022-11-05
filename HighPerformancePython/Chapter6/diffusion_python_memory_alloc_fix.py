d0453debe2c62b5d60c2d84e530b29318a93da9f
Ankush-Chander <ankush.watchtower@gmail.com>
2022-11-05 11:09:20 +0530

    profile memory allocation.

---
+++ HighPerformancePython/Chapter6/diffusion_python_memory_alloc_fix.py
@@ -0,0 +1,59 @@
+import time
+import traceback
+
+from icecream import ic
+from line_profiler import LineProfiler
+
+lp = LineProfiler()
+grid_shape = (640, 640)
+
+
+# profile is added in builtin namespace when run via kernprof
+# usage:  kernprof -l -v diffusion_python.py
+@profile
+def evolve(grid, dt, out, D=1.0):
+    xmax, ymax = grid_shape
+
+    for i in range(xmax):
+        for j in range(ymax):
+            try:
+
+                # ic(grid)
+                grid_xx = (grid[(i + 1) % xmax][j] + grid[(i - 1) % xmax][j] - 2.0 * grid[i][j])
+                grid_yy = (grid[i][(j + 1) % ymax] + grid[i][(j - 1) % ymax] - 2.0 * grid[i][j])
+                out[i][j] = grid[i][j] + D * (grid_xx + grid_yy) * dt
+            except Exception as err:
+                print(traceback.format_exc())
+                # ic((i + 1) % xmax)
+                # ic(grid)
+                return
+    return out
+
+
+def run_experiment(num_iterations):
+    # setting initial conditions
+    xmax, ymax = grid_shape
+    next_grid = [[0.0] * ymax for x in range(xmax)]
+    grid = [[0.0] * ymax for x in range(xmax)]
+    # ic(len(grid))
+    # Drop a dye in the middle
+    block_low = int(grid_shape[0] * 0.4)
+    block_high = int(grid_shape[0] * 0.5)
+    for x in range(block_low, block_high):
+        for y in range(block_low, block_high):
+            grid[x][y] = 0.005
+
+    # evolve initial conditions
+    start = time.time()
+    # ic(grid)
+    for i in range(num_iterations):
+        ic(i)
+        ic(type(grid))
+        grid = evolve(grid, 0.1, next_grid)
+        grid, next_grid = next_grid, grid
+
+    return time.time() - start
+
+
+if __name__ == '__main__':
+    run_experiment(num_iterations=100)
